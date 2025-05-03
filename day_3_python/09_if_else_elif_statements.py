7f797234bb916f165a658cc7a686fce847d82abc
Asmaa Masood <tasawar0756@gmail.com>
2025-05-03 10:43:10 +0500

    Added material up to day 3

---
+++ day_3_python/09_if_else_elif_statements.py
@@ -0,0 +1,9 @@
+#Conditionals
+required_age_at_school=5
+hammad_age=int(input("what is your age? "))
+if hammad_age==required_age_at_school:
+    print("congratulation! Hammad can join the school.")
+elif hammad_age< required_age_at_school:
+    print("Hammad is still a baby.")
+else:
+    print("Hammad shoud join the higher school")
